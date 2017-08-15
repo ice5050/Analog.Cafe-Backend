e7357e2fab1ec41ce23ff69576b0234b82fe5f2c
akiyama-eve <tawan@bananacoding.com>
2017-08-15 12:36:23 +0700

    submission api

--- app/submission/index.js
+++ app/submission/index.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const passport = require('passport')
+const count = require('word-count')
+const slugify = require('slugify')
 const Submission = require('../../models/mongo/submission.js')
 const submissionApp = express()
 
@@ -16,24 +19,69 @@ submissionApp.get('/submissions/:submissionId', (req, res) => {
     })
 })
 
-submissionApp.post('/submissions', (req, res) => {
+function raw2Text(raw){
+  var text = ""
+  for(var i = 0; i < raw.document.nodes.length; i++ ){
+    var nodeI = raw.document.nodes[i]
+    text = text + " " // new line
+    for(var j = 0; j < nodeI.nodes.length; j++ ){
+      var nodeJ = nodeI.nodes[j]
+      for(var k = 0; k < nodeJ.ranges.length; k++ ){
+        var ranges = nodeJ.ranges[k]
+        text = text + ranges.text
+      }
+    }
+  }
+  return text
+}
+
+function rawImageCount(raw){
+  var imgCount = 0
+  for(var i = 0; i < raw.document.nodes.length; i++ ){
+    if(raw.document.nodes[i].type == "image"){
+      imgCount++
+    }
+  }
+  return imgCount
+}
+
+function randomString(length)
+{
+    var text = "";
+    var charset = "abcdefghijklmnopqrstuvwxyz0123456789";
+    for( var i=0; i < length; i++ ){
+      text += charset.charAt(Math.floor(Math.random() * charset.length));
+    }
+    return text;
+}
+
+
+function slugGenerator(str){
+  return slugify(str) + randomString(4)
+}
+
+submissionApp.post('/submissions', passport.authenticate('jwt', { session: false }), (req, res) => {
+  var rawObj = req.body.raw
+  var rawText = raw2Text(rawObj)
   const newSubmission = new Submission({
-    slug: req.body.slug,
+    slug: slugGenerator(req.body.title),
     title: req.body.title,
     subtitle: req.body.subtitle,
     stats: {
-      images: req.body.images,
-      words: req.body.words
+      images: rawImageCount(rawObj),
+      words: count(rawText)
     },
     author: req.user.id,
-    articleId: req.body.articleId,
     poster: {
-      small: req.body.poster.small,
-      medium: req.body.poster.medium,
-      large: req.body.poster.large
+      // small: req.body.poster.small,
+      // medium: req.body.poster.medium,
+      // large: req.body.poster.large
+      small: "",
+      medium: "",
+      large: ""
     },
-    summary: req.body.summary,
-    content: req.body.content
+    summary: rawText.substring(0,250),
+    content: { raw: rawObj }
   })
   newSubmission.save().then(submission => {
     res.json({ data: submission })
