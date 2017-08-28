75cd58762c2711925b3d3c7e9ea38c7fe61478dd
Ice5050 <ice50505050@hotmail.com>
2017-08-28 14:19:01 +0700

    Fix list api when author does not exist
    and refactor list api

--- app/article/index.js
+++ app/article/index.js
@@ -54,42 +54,27 @@ articleApp.get(['/articles', '/list'], async (req, res) => {
     .skip(itemsPerPage * (page - 1))
     .sort({ 'post-date': 'desc' })
 
-  query.exec((err, articles) => {
-    countQuery.count().exec((err, count) => {
-      if (author) {
-        User.findOne({ id: author }).then(user => {
-          res.json({
-            status: 'ok',
-            filters: {
-              tags: tags,
-              author: { id: user.id, name: user.title }
-            },
-            page: {
-              current: page,
-              total: Math.ceil(count / itemsPerPage),
-              'items-total': count,
-              'items-per-page': itemsPerPage
-            },
-            items: articles
-          })
-        })
-      } else {
-        res.json({
-          status: 'ok',
-          filters: {
-            tags: tags,
-            author: {}
-          },
-          page: {
-            current: page,
-            total: Math.ceil(count / itemsPerPage),
-            'items-total': count,
-            'items-per-page': itemsPerPage
-          },
-          items: articles
-        })
-      }
-    })
+  const articles = await query.exec()
+  const count = await countQuery.count().exec()
+  let user
+  if (author) {
+    user = await User.findOne({ id: author }).exec()
+  }
+  res.json({
+    status: 'ok',
+    filter: {
+      tags: tags,
+      author: author
+        ? { id: author, name: (user && user.title) || '' }
+        : undefined
+    },
+    page: {
+      current: page,
+      total: Math.ceil(count / itemsPerPage),
+      'items-total': count,
+      'items-per-page': itemsPerPage
+    },
+    items: articles
   })
 })
 
@@ -98,34 +83,5 @@ articleApp.get('/articles/:articleSlug', (req, res) => {
     res.json(article)
   })
 })
-
-// articleApp.post('/articles/:articleId', (req, res) => {
-//   Article.findOne({ articleId: req.params.articleId }).then(article => {
-//     article = {
-//       ...article,
-//       ...{
-//         title: req.body.title,
-//         subtitle: req.body.subtitle,
-//         stats: {
-//           images: req.body.images,
-//           words: req.body.words
-//         },
-//         poster: {
-//           small: req.body.poster.small,
-//           medium: req.body.poster.medium,
-//           large: req.body.poster.large
-//         },
-//         repostOK: req.body['repost-ok'],
-//         tag: req.body.tag,
-//         status: req.body.status,
-//         summary: req.body.summary,
-//         content: req.body.content
-//       }
-//     }
-//     return article.save()
-//   }).then(article => {
-//     res.json({ data: article })
-//   })
-// })
 
 module.exports = articleApp
