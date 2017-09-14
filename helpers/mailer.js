49ce740ed5bd7d0ac673984384f252625a67627a
Ice5050 <ice50505050@hotmail.com>
2017-09-14 17:36:13 +0700

    Fix mailer

--- helpers/mailer.js
+++ helpers/mailer.js
@@ -1,22 +1,15 @@
-let SibApiV3Sdk = require('sib-api-v3-sdk')
-let defaultClient = SibApiV3Sdk.ApiClient.instance
-
-let apiKey = defaultClient.authentications['api-key']
-apiKey.apiKey = process.env.SENDINBLUE_API_KEY
-
-let apiInstance = new SibApiV3Sdk.SMTPApi()
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 function sendMail (data) {
-  let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail({
-    sender: { name: data.from, email: data.from },
-    to: [data.to],
-    replyTo: { email: data.from },
+  const msg = {
+    to: data.to,
+    from: data.from,
     subject: data.subject,
-    htmlContent: data.html
-  })
-  apiInstance
-    .sendTransacEmail(sendSmtpEmail)
-    .then(data => console.log(data), error => console.error(error))
+    text: data.text,
+    html: data.html
+  }
+  sgMail.send(msg)
 }
 
 function sendVerifyEmail (to, verifyCode, verifyLink) {
