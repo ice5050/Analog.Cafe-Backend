6b8fb72a43f7275fe61835033582ddcef046fec0
Ice5050 <ice50505050@hotmail.com>
2017-09-19 17:41:56 +0700

    Admin can delete image

--- app/image/index.js
+++ app/image/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const passport = require('passport')
 const Image = require('../../models/mongo/image')
 const imageApp = express()
 
@@ -13,4 +14,22 @@ imageApp.get('/images/:imageId', async (req, res) => {
     id: image.id
   })
 })
+
+// Delete image (only admin)
+imageApp.put(
+  '/images/:imageId/delete',
+  passport.authenticate('jwt', { session: false }),
+  async (req, res) => {
+    if (req.user.role !== 'admin') {
+      return res.status(401).json({ message: 'No permission to access' })
+    }
+    let image = await Image.findOne({ id: req.params.imageId })
+    if (!image) {
+      return res.status(404).json({ message: 'Image not found' })
+    }
+    image = await image.remove()
+    res.json({ status: 'ok' })
+  }
+)
+
 module.exports = imageApp
