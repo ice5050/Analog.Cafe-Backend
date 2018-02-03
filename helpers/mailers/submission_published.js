2b3d240f07b58bee4e95709a6bf245c0df35a5df
Ice5050 <ice50505050@hotmail.com>
2018-02-03 16:50:30 +0700

    [Added] Sending email functions for submission being published and rejected [Removed] the function for sending submission status being updated email

---
+++ helpers/mailers/submission_published.js
@@ -0,0 +1,22 @@
+const { sendMail } = require('../mailer')
+
+/**
+ * send an email to the user about his submission being approved
+ *
+ * @param {object} author - author with email and title
+ * @param {object} article - article with slug
+ */
+function submissionPublishedEmail (author, article) {
+  sendMail({
+    to: author.email,
+    from: { email: 'info@analog.cafe', name: 'Analog.Cafe' },
+    subject: 'Your submission has been published',
+    templateId: '3395e29a-4ef7-48ae-9056-c6c037ff2461',
+    substitutions: {
+      'first_name | there': author.title,
+      'article_url': `${process.env.ANALOG_FRONTEND_URL}/zine/${article.slug}`
+    }
+  })
+}
+
+module.exports = submissionPublishedEmail
