b5d34b6306c28dab01898a55f7ac202d864e0f53
Ice5050 <ice50505050@hotmail.com>
2018-08-21 10:10:42 +0700

    [Added] Constraint to prevent user email duplication

--- models/mongo/user.js
+++ models/mongo/user.js
@@ -11,7 +11,7 @@ const userSchema = new Schema({
   text: String,
   twitterId: String,
   facebookId: String,
-  email: String,
+  email: { type: String, lowercase: true, unique: true },
   role: { type: String, default: 'member' },
   buttons: [
     {
