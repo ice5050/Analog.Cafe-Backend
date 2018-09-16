d0c08155829a1eaa1c08c0bf4ead0053011167f7
Dmitri <d@eurekaking.com>
2018-09-16 14:34:00 -0700

    Update copy & provide all published authors' list

--- app/author/index.js
+++ app/author/index.js
@@ -5,6 +5,46 @@ const authorApp = express()
 
 /**
  * @swagger
+ * /authors:
+ *   get:
+ *     description: Get all published authors
+ *     responses:
+ *       200:
+ *         description: Return authors.
+ */
+authorApp.get('/authors', async (req, res) => {
+  const page = req.query.page || 1
+  const itemsPerPage = Number(req.query['items-per-page']) || 10
+  const condition = {
+    $or: [{ role: 'contributor' }, { role: 'admin' }, { role: 'editor' }]
+  }
+
+  let queries = [User.find(condition), User.find(condition)]
+  let [query, countQuery] = queries
+
+  query
+    .select('id title image text role buttons')
+    .limit(itemsPerPage)
+    .skip(itemsPerPage * (page - 1))
+    .cache(300)
+
+  const users = await query.exec()
+  const count = await countQuery.count().exec()
+
+  res.json({
+    status: 'ok',
+    page: {
+      current: page,
+      total: Math.ceil(count / itemsPerPage),
+      'items-total': count,
+      'items-per-page': itemsPerPage
+    },
+    items: users
+  })
+})
+
+/**
+ * @swagger
  * /authors/:author:
  *   get:
  *     description: Get author by id
