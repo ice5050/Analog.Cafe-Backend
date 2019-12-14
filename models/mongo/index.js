b962da8a3f566e2952a526a879cf9494be5616c4
dmitrizzle <8587882+dmitrizzle@users.noreply.github.com>
2019-12-14 10:39:58 -0800

    Fix depreciation warnings

--- models/mongo/index.js
+++ models/mongo/index.js
@@ -5,6 +5,14 @@ const cachegoose = require('cachegoose')
 cachegoose(mongoose, process.env.REDIS_URL)
 
 mongoose.Promise = Promise
+
+// get past depreciation warning
+// https://mongoosejs.com/docs/deprecations.html
+mongoose.set('useFindAndModify', false)
+mongoose.set('useCreateIndex', true)
+mongoose.set('useUnifiedTopology', true)
+mongoose.set('useNewUrlParser', true)
+
 const connection = mongoose.createConnection(
   process.env.MONGODB_URI || 'mongodb://localhost:27017/analogCafeDev'
 )
