cfe5f75018706fcae51c277fe2d106db6e63e9c5
dmitrizzle <8587882+dmitrizzle@users.noreply.github.com>
2020-10-12 20:32:46 -0700

    Remove defunct test

--- helpers/authenticate.test.js
+++
@@ -1,6 +0,0 @@
-
-const { sanitizeUsername, getProfileImageURL } = require("./authenticate");
-
-test("Convert case-sensitive emails to lowercase usernames", ()=> {
-  // expect(sanitizeUsername("UsEr@email.com")).toEqual("user")
-})
