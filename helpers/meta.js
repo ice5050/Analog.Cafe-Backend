192243a510e1190fad9bb01568b8c38ef03a8610
dmitrizzle <8587882+dmitrizzle@users.noreply.github.com>
2021-04-29 21:46:59 -0700

    Update scrub message

--- helpers/meta.js
+++ helpers/meta.js
@@ -2,7 +2,7 @@ const scrubSummary = summary =>
   summary
     ? summary
         .replace(
-          "Get Community Letters (articles like this) monthly, via email.",
+          "Get “Community Letters” via email: a monthly overview of the latest news, events, and stories from the film photography community.",
           ""
         )
         .replace("☞", "")
